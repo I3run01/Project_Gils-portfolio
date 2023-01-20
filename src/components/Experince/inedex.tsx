c45622b586d089d8aa7e8dd763baf4b1b5834639
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-01-20 09:47:46 -0300

    c

--- src/components/Experince/inedex.tsx
+++ src/components/Experince/inedex.tsx
@@ -1,10 +1,15 @@
 import { ExperienceStyled } from './styled'
+import { Jobs } from './Classes/classExperience'
 
 export const Experince = () => {
 
     return (
         <ExperienceStyled>
+            <h1>Experiência</h1>
             
+            {
+                Jobs.map()
+            }
         </ExperienceStyled>
     )
 }
