298e290e634790cd4e486600a1334f5955d9b434
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-01-20 20:55:58 -0300

    c

--- src/components/Experince/index.tsx
+++ src/components/Experince/index.tsx
@@ -19,6 +19,18 @@ export const Experince = () => {
         return `${years}a e ${remainingMonths}m`;
 
     }
+
+    function findSmallestIndex(arr:number[]) {
+        let smallest = arr[0];
+        let smallestIndex = 0;
+        for (let i = 1; i < arr.length; i++) {
+          if (arr[i] < smallest) {
+            smallest = arr[i];
+            smallestIndex = i;
+          }
+        }
+        return smallestIndex;
+    }
       
     return (
         <ExperienceStyled>
