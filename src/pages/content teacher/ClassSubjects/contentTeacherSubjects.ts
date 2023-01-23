8f45c3a291732395f8b363b7b8c76290e4e357a8
I3run01 <brunnooa.v@gmail.com>
2023-01-23 19:59:55 -0300

    c

--- src/pages/content teacher/ClassSubjects/contentTeacherSubjects.ts
+++ src/pages/content teacher/ClassSubjects/contentTeacherSubjects.ts
@@ -2,19 +2,28 @@ type gallary = {
     image: string
 }[]
 
+type colors = {
+    colorRight_1: string
+    colorRight_2: string
+    colorLeft_1: string
+    colorLeft_2: string
+}
+
 export class contentTeacherSubjects {
     private _title: string;
     private _icon: string;
     private _image: string;
     private _description: string;
     private _gallary: gallary;
+    private _colors: colors
 
-    constructor(title: string, icon: string, image: string, description: string, gallary: gallary) {
+    constructor(title: string, icon: string, image: string, description: string, gallary: gallary, colors: colors) {
         this._title = title;
         this._icon = icon;
         this._image = image;
         this._description = description;
         this._gallary = gallary;
+        this._colors = colors
     }
 
     get title(): string {
@@ -36,4 +45,17 @@ export class contentTeacherSubjects {
     get gallary(): gallary {
         return this._gallary;
     }
+
+    get colors(): colors {
+        return this._colors;
+    }
 }
+
+export const contentTeacherObj = [
+    new contentTeacherSubjects(
+        'Robótica',
+        'contentTeacher/roticsIcon.svg',
+        'contentTeacher/roticsImage.png',
+        
+    )
+]
