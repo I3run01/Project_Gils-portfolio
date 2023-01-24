7112ea8e2c428211f380c1ae77d128cd5ebd2187
I3run01 <brunnooa.v@gmail.com>
2023-01-24 19:01:41 -0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { LeftMenuBar } from './components/GlobalComponents/leftMenuBar'
 import { RightMenuBar } from './components/GlobalComponents/rightMenuBar'
 import { BackGround } from './components/GlobalComponents/bk'
+import { Footer } from './components/GlobalComponents/footer'
 import { GlobalStyled } from './globalStyled'
 import { MainRoutes } from './routes/mainRoutes'
  
@@ -12,6 +13,7 @@ function App() {
       <RightMenuBar/>
       <LeftMenuBar/>
       <MainRoutes/>
+      <Footer/>
     </GlobalStyled>
   )
 }
--- src/components/GlobalComponents/footer/styled.tsx
+++ src/components/GlobalComponents/footer/styled.tsx
@@ -1,7 +1,22 @@
 import styled from "styled-components";
 
-export const FooterStyled = styled.div`
-    
+type props = {
+    theme: 'dark' | 'light'
+}
 
-    
+const fontColorDark = '#d2d2d2'
+const fontColorLight = 'black'
+
+export const FooterStyled = styled.div<props>`
+    min-height: 50px;
+    width: max-content;
+
+    box-sizing: border-box;
+    background: linear-gradient(179.21deg, rgba(255, 253, 253, 0.25) 0%, rgba(255, 255, 255, 0.15) 100%);
+    box-shadow: 0px 4px 24px -1px rgba(0, 0, 0, 0.25);
+    backdrop-filter: blur(40px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 20px;
+    border: 3px solid ${props => props.theme === 'dark' ? fontColorDark : fontColorLight};
+
 `
