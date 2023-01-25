4985a8ad114acc50d23280e9c8f29f726831718c
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-01-25 20:47:11 -0300

    c

--- src/pages/gilsServices/servicesClass/servicesClass.ts
+++ src/pages/gilsServices/servicesClass/servicesClass.ts
@@ -51,8 +51,8 @@ export class servicesClass {
 export const servicesObj = [
     new servicesClass (
         'Excel',
-        'Services/Excel/excelIcon.svg',
-        'Services/Excel/ExcelImage.png',
+        'Gils Services/Excel/excelIcon.svg',
+        'Gils Services/Excel/ExcelImage.png',
         `Excel é uma planilha eletrônica desenvolvida pela Microsoft. Ele permite criar e editar planilhas com cálculos, gráficos e tabelas, além de oferecer diversas ferramentas de análise de dados e de automação de tarefas. É amplamente utilizado para gerenciamento de dados, análise financeira e outras tarefas de negócios.`,
         [],
         {
@@ -64,15 +64,16 @@ export const servicesObj = [
     ),
     new servicesClass (
         'Manuais de robótica',
-        'Services/Excel/robot manual icon.svg',
-        'Services/Excel/ExcelImage.png',
-        `Excel é uma planilha eletrônica desenvolvida pela Microsoft. Ele permite criar e editar planilhas com cálculos, gráficos e tabelas, além de oferecer diversas ferramentas de análise de dados e de automação de tarefas. É amplamente utilizado para gerenciamento de dados, análise financeira e outras tarefas de negócios.`,
+        'Gils Services/Robot Manual/robot manual icon.svg',
+        'Gils Services/Robot Manual/Robot Image.png',
+        `Um manual de robótica é um documento que fornece instruções detalhadas sobre como construir, programar e operar um robô. Ele pode incluir informações sobre os componentes do robô, como sensores, atuadores e controladores, bem como informações sobre como programar e controlar o robô. Um manual também pode incluir exemplos de aplicações e projetos para ajudar os usuários a entender melhor como usar o robô. Ele é útil para iniciantes que querem aprender a construir e programar robôs, assim como para profissionais que estão trabalhando em projetos de robótica avançados.
+        `,
         [],
         {
-            colorRight_1: '#03733F',
-            colorRight_2: '#365943',
-            colorLeft_1: '#F2916D',
-            colorLeft_2: '#BF4A3F',
+            colorRight_1: '#F2EEB6',
+            colorRight_2: '#BF754B',
+            colorLeft_1: '#F24405',
+            colorLeft_2: '#A65F46',
         }
     )
     
